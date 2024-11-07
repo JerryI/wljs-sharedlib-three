e4c96cad7b74d3ccf78030540f0cd32520ed03a8
Kirill Vasin <krikus.ms@gmail.com>
2024-11-07 19:25:47 +0100

    update

--- dist/kernel.js
+++ dist/kernel.js
@@ -2,8 +2,9 @@ const loader = async (self) => {
     self.THREE         = (await import('./three.module-494e6c91.js'));
     self.OrbitControls = (await import('./OrbitControls-199dbeff.js')).OrbitControls;
     self.RGBELoader    = (await import('./RGBELoader-dc5102ec.js')).RGBELoader; 
+    self.CSS2D         = (await import('./labels-7119234c.js'));
     
-    console.log('THREE shared library loaded!');
+    console.log('THREE shared library loader!');
   };
 
   const loader1 = async (self) => {
@@ -17,6 +18,24 @@ const loader = async (self) => {
   const loader3 = async (self) => {
     self.PointerLockControls = (await import('./PointerLockControls-a3da7dd0.js')).PointerLockControls;
   };
+
+  const loader4 = async (self) => {
+    self.RTX = (await import('./index.module-9175e3c5.js'));
+  };
+
+  const loader5 = async (self) => {
+    const engine = await import('./CSS2DRenderer-469b8bc0.js');
+    self.CSS2DRenderer = engine.CSS2DRenderer;
+    self.CSS2DObject = engine.CSS2DObject;
+    //import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js'
+  };
+
+  const loader6 = async (self) => {
+    const sprite = await import('./three-spritetext-4fb23878.js');
+    self.SpriteText = sprite;
+  };
+
+  
 
   new interpretate.shared(
     "THREE",
@@ -36,4 +55,19 @@ const loader = async (self) => {
   new interpretate.shared(
     "THREEPointerLockControls",
     loader3
+  );
+
+  new interpretate.shared(
+    "THREERTX",
+    loader4
+  );
+
+  new interpretate.shared(
+    "THREECSS",
+    loader5
+  );  
+
+  new interpretate.shared(
+    "SpriteText",
+    loader6
   );
